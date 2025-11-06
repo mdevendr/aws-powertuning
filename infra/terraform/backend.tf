dc564cb6137fe015c7c1ab1f9a0b83f81424fa10
mdevendr <mahesh.devendran@gmail.com>
2025-11-06 06:00:04 +0000

    initial

---
+++ infra/terraform/backend.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  backend "s3" {
+    bucket         = "serverless-power-tuning-tfstate"
+    key            = "state/terraform.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "serverless-power-tuning-tf-lock"
+    encrypt        = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
