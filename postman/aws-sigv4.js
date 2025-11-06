f1bff119d6ea52512d79c54db63fb12263084d6d
mdevendr <mahesh.devendran@gmail.com>
2025-11-06 20:03:16 +0000

    testing

--- postman/aws-sigv4.js
+++
@@ -1,9 +0,0 @@
-const aws4 = require('aws4');
-
-pm.request = aws4.sign(pm.request, {
-  accessKeyId: pm.environment.get('awsAccessKeyId'),
-  secretAccessKey: pm.environment.get('awsSecretAccessKey'),
-  sessionToken: pm.environment.get('awsSessionToken'),
-  service: 'execute-api',
-  region: 'eu-west-2'
-});
