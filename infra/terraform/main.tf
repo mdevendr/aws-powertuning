60097a829aba4d2402ea9e65f15a5a47c61953be
mdevendr <mahesh.devendran@gmail.com>
2025-11-06 21:51:05 +0000

    adding apigw methods support

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -144,6 +144,38 @@ resource "aws_api_gateway_integration" "item" {
   uri                     = aws_lambda_function.api.invoke_arn
 }
 
+resource "aws_api_gateway_resource" "item_id" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_resource.items.id
+  path_part   = "{id}"
+}
+
+resource "aws_api_gateway_method" "item_id_any" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.item_id.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "item_id" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.item_id.id
+  http_method             = aws_api_gateway_method.item_id_any.http_method
+  type                    = "AWS_PROXY"
+  integration_http_method = "POST"
+  uri                     = aws_lambda_function.api.invoke_arn
+}
+
+# Ensure deployment waits for new integration
+resource "aws_api_gateway_deployment" "deploy" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  depends_on = [
+    aws_api_gateway_integration.items,
+    aws_api_gateway_integration.item_id
+  ]
+}
+
+
 resource "aws_lambda_permission" "apigw" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.api.function_name
