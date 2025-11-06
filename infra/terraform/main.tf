016903faa84ff6e28245e0e3745f6ad07806c7f1
mdevendr <mahesh.devendran@gmail.com>
2025-11-06 21:57:42 +0000

    adding apigw methods support

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -175,7 +175,7 @@ resource "aws_lambda_permission" "apigw" {
   source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
 }
 
-resource "aws_api_gateway_deployment" "deploy2" {
+resource "aws_api_gateway_deployment" "deploy" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   depends_on = [
     aws_api_gateway_integration.items,
