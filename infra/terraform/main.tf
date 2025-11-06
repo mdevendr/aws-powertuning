028e73c27128d9bf6506f9fb865ddad51bc58f61
mdevendr <mahesh.devendran@gmail.com>
2025-11-06 22:14:32 +0000

    adding apigw methods support

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -89,8 +89,9 @@ resource "aws_lambda_function" "api" {
 }
 
 ########################################
-# API Gateway
+# API Gateway Setup
 ########################################
+
 resource "aws_api_gateway_rest_api" "api" {
   name = "${var.project}-api"
 }
@@ -102,6 +103,7 @@ resource "aws_api_gateway_resource" "items" {
   path_part   = "items"
 }
 
+# ANY /items
 resource "aws_api_gateway_method" "items_any" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
   resource_id   = aws_api_gateway_resource.items.id
@@ -118,38 +120,14 @@ resource "aws_api_gateway_integration" "items" {
   uri                     = aws_lambda_function.api.invoke_arn
 }
 
-# --------------------------- #
-# ADDED: /items/{id} resource #
-# --------------------------- #
-resource "aws_api_gateway_resource" "item" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  parent_id   = aws_api_gateway_resource.items.id
-  path_part   = "{id}"
-}
-
-# ANY /items/{id} (proxy to the same Lambda)
-resource "aws_api_gateway_method" "item_any" {
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.item.id
-  http_method   = "ANY"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "item" {
-  rest_api_id             = aws_api_gateway_rest_api.api.id
-  resource_id             = aws_api_gateway_resource.item.id
-  http_method             = aws_api_gateway_method.item_any.http_method
-  type                    = "AWS_PROXY"
-  integration_http_method = "POST"
-  uri                     = aws_lambda_function.api.invoke_arn
-}
-
+# /items/{id}
 resource "aws_api_gateway_resource" "item_id" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   parent_id   = aws_api_gateway_resource.items.id
   path_part   = "{id}"
 }
 
+# ANY /items/{id}
 resource "aws_api_gateway_method" "item_id_any" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
   resource_id   = aws_api_gateway_resource.item_id.id
@@ -166,8 +144,9 @@ resource "aws_api_gateway_integration" "item_id" {
   uri                     = aws_lambda_function.api.invoke_arn
 }
 
-# Ensure deployment waits for new integration
-
+########################################
+# Permissions
+########################################
 resource "aws_lambda_permission" "apigw" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.api.function_name
@@ -175,25 +154,25 @@ resource "aws_lambda_permission" "apigw" {
   source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
 }
 
+resource "aws_lambda_permission" "allow_stepfn" {
+  statement_id  = "AllowExecution"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.api.function_name
+  principal     = "states.amazonaws.com"
+  source_arn    = var.power_tuner_arn
+}
+
+########################################
+# Deployment + Stage
+########################################
 resource "aws_api_gateway_deployment" "deploy" {
   rest_api_id = aws_api_gateway_rest_api.api.id
 
-  # ADDED: include the new /items/{id} integration so changes trigger redeploy
   depends_on = [
     aws_api_gateway_integration.items,
-    aws_api_gateway_integration.item
+    aws_api_gateway_integration.item_id
   ]
 }
-
-# resource "aws_api_gateway_deployment" "deploy" {
-#   rest_api_id = aws_api_gateway_rest_api.api.id
-#   depends_on = [
-#     aws_api_gateway_integration.items,
-#     aws_api_gateway_integration.item_id
-#   ]
-# }
-
-
 
 resource "aws_api_gateway_stage" "prod" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
@@ -202,23 +181,9 @@ resource "aws_api_gateway_stage" "prod" {
 }
 
 ########################################
-# Reference Existing SAR Power Tuner (Manual Deploy)
-########################################
-
-resource "aws_lambda_permission" "allow_stepfn" {
-  statement_id  = "AllowExecution"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.api.function_name
-  principal     = "states.amazonaws.com"
-  source_arn    = "arn:aws:states:us-west-2:211125489043:stateMachine:powerTuningStateMachine-8d6ae210-bb08-11f0-98f3-0656addddf6b"
-  # the ARN will be replaced with a more dynamic reference in future updates after SAR implementation from this pipeline
-  # The SAR implementation will be replaced by a more robust and configurable automation in future updates
-  # for now, this is a manual step to link the Lambda function with the existing Power Tuner State Machine
-}
-
-########################################
 # Outputs
 ########################################
+
 output "invoke_url" {
   value = "https://${aws_api_gateway_rest_api.api.id}.execute-api.${var.aws_region}.amazonaws.com/${aws_api_gateway_stage.prod.stage_name}"
 }
